c8620679ce3ccf7a1b4b4d4ab2ee9f7df7566287
ulydev <ulydev@live.fr>
2020-02-27 14:22:12 +0100

    initial commit

---
+++ app/src/main/java/sdp/epfl/ch/bootcamp/MainActivity.java
@@ -0,0 +1,13 @@
+package sdp.epfl.ch.bootcamp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
